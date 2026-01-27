477312ca751c8589809d5ca5d3e06e36dcb4f067
Rahmat Ardiansyah <artsbymat@gmail.com>
2026-01-27 17:59:52 +0700

    style: move button to dropdown for toggle theme

--- src/app/(vendor)/layout.tsx
+++ src/app/(vendor)/layout.tsx
@@ -12,9 +12,14 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { ModeToggle } from "@/components/ui/theme-toggle";
 import { AppSidebar } from "@/components/vendor/sidebar";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function VendorLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -22,6 +27,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
+            <ModeToggle />
             <Separator
               orientation="vertical"
               className="mr-2 data-[orientation=vertical]:h-4"
